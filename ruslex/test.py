91fbed19bd420417aa4a3473774fdcb99b912510
Yulia Spektor <yuliaspektor@gmail.com>
2021-02-18 17:11:32 -0500

    updated test

--- ruslex/test.py
+++ ruslex/test.py
@@ -5,5 +5,25 @@ from ruslex import clexicon
 lexicon = clexicon.wordform_lexicon()
 
 for key in lexicon.analyses:
-	if key == 'ребята':
+	if key == "Августович":
+		print("Data format:")
 		print(lexicon.analyses[key])
+		for analysis in lexicon.analyses[key].analysis:
+			print("Data source:", analysis.source)
+			print()
+			print("All morphological features:", analysis.morphology.morph_features)
+			print("Individual morphological features:")
+			for feature in analysis.morphology.morph_features:
+				print(feature)
+			print()
+			for pron in analysis.pronunciation:
+				print("Pronunciation (phonetic):", pron.phone)
+				print("Phones:")
+				for phone in pron.phone:
+					print(phone)
+				print()
+				print("Pronunciation (orthographic with stress):", pron.segment)
+				print("Segments (with stress):")
+				for segment in pron.segment:
+					print(segment)
+				print()
